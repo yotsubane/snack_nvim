17c5dcb63f5fe755bd11a1d04b93c6f708d57aff
De CARVALHO David <david.decarvalho@gieargos.org>
2025-05-19 17:29:30 +0200

    oui'

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -96,3 +96,13 @@ require("mini.completion").setup({
 	},
 	set_vim_settings = true,
 })
+
+require("nvim-treesitter.configs").setup({
+	ensure_installed = "all",
+	sync_install = true,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
